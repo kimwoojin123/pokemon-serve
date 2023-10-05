52a707ed508e3c0f71247c29ddf9a63823404574
kimwoojin <kimwoojin1903@gmail.com>
2023-10-05 10:57:34 +0900

    서버에 문서 작성

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const http = require('http');
 
 const server = http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type' : 'text/html'});
-  res.end()
+  
+  let doc = `<html> <head> </head> <body> <h1> hello </h1> </body> </html>`
+  
+  res.end(doc)
 });
 
 server.listen(1234);
