a2c2c32159b4a3f7be96f8ad1a19b7f4974be8fc
kimwoojin <kimwoojin1903@gmail.com>
2023-10-05 15:25:01 +0900

    메인페이지 서브페이지 작성, 서로 이동기능 추가

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const http = require('http');
 
 const server = http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type' : 'text/html'});
-  
-  let doc = `<html> <head> </head> <body> <h1> hello </h1> </body> </html>`
-  
-  res.end(doc)
-});
+  res.writeHead(200, {'Content-Type' : 'text/html;charset=utf8'});
+  if(req.url === "/"){
+    let doc = `<html> <head> </head> <body> <h1> <a href = "http://localhost:1234/sub"> 메인입니다. </a> </h1> </body> </html>`
+    res.end(doc)
+  }
+  if(req.url === "/sub"){
+    let doc1 = `<html> <head> </head> <body> <h1> <a href = "http://localhost:1234"> 서브입니다. </a> </h1> </body> </html>`
+    res.end(doc1)
+  }
+})
+server.listen(1234)
 
-server.listen(1234);
