c9a47fec1057b8f9b8ffe651979c4783af2f43ad
kimwoojin <kimwoojin1903@gmail.com>
2023-10-05 16:48:26 +0900

    페이지이동시 로딩기능 추가

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 const http = require('http');
 
+
 const server = http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type' : 'text/html;charset=utf8'});
+  res.writeHead(200, {'Content-Type' : 'text/html;charset=utf-8'});
   if(req.url === "/"){
-    let doc = `<html> <head> </head> <body> <h1> <a href = "http://localhost:1234/sub"> 메인입니다. </a> </h1> </body> </html>`
+    let doc = 
+    `<html>
+    <head>
+    </head>
+    <body>
+    <a onClick="main()"> 메인입니다. </a>
+    <script> 
+    function main() {
+      setTimeout(function(){
+        location.href = "http://localhost:1234/sub"
+      },2000)
+    }
+    </script>
+    </body> </html>`
     res.end(doc)
   }
   if(req.url === "/sub"){
-    let doc1 = `<html> <head> </head> <body> <h1> <a href = "http://localhost:1234"> 서브입니다. </a> </h1> </body> </html>`
+    let doc1 = 
+    `<html> 
+    <head> 
+    </head> 
+    <body> 
+    <a onclick = "main()"> 서브입니다. </a> 
+    <script>
+    function main() {
+      setTimeout(function(){
+        location.href = "http://localhost:1234"
+      },2000)
+    }
+    </script>
+    </body> 
+    </html>`
     res.end(doc1)
   }
 })
+
 server.listen(1234)
 
